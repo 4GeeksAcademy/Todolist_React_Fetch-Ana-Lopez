278f645898b5d64d78492a46dc47d7b67e0e8b0d
Ana Lopez <avantadesignlab@gmail.com>
2026-03-31 02:45:44 +0000

    to do list api, lista de tareas y crear usuario

---
+++ src/js/components/TodolistApi.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+export const TodolistApi = () => {
+
+
+    //estado para guardar el array pe dersonajes (inicialmente esta vacio)
+    const [lista, setLista] = useState([])
+    const [tarea, setTarea] = useState("")
+
+    const API_URL = "https://playground.4geeks.com/todo"
+
+    const creasrUsuario = () => {
+
+        fetch(API_URL + "/users/astrid", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json()) //conviuerte la respuesta a un formato JSON
+            .then(data => { console.log(data) })  //toma los datos para mostrar en el array
+            .catch(error => {
+                console.error("Hubo un problema al crear el usuario", error); //imprimir el error enn la consola para depurar
+            })
+    }
+
+    const obtenerLista = () => {
+
+        fetch(API_URL + "/users/astrid")
+            .then((response) => {
+                if (response.status === 404) {
+                    crearUsuario()
+                }
+                return response.json()
+            })
+            .then(data => { setLista(data.todos) })  //toma los datos para mostrar en el array
+            .catch(error => {
+                console.error("Hubo un problema al obtener la lista de tareas", error); //imprimir el error enn la consola para depurar
+            })
+    }
+
+    //      useEffect(()=>{
+    //    aqui va el codigo que hace algo (ejemplo el fetch)
+    //       },[ cuando se repite, si esta vacio se ejecuta solo una vez al cargar la pagina, si tiene un dato se ejecuta cada vez que ese dato cambie ])
+
+
+    useEffect(() => {
+        obtenerLista()
+    }, [])
+
+
+    return (
+        <div>
+            <h1>Tareas de NahYah</h1>
+
+            <ol>
+                {lista.map((item) => (
+                    <li key={item.id}> {item.label}</li>
+                ))}
+            </ol>
+        </div>
+    )
+
+}
