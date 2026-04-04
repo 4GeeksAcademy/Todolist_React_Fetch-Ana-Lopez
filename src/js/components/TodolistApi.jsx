c0fc33e94b7c80b8e3067e75dd2b5fb5157a3495
Ana Lopez <avantadesignlab@gmail.com>
2026-04-04 02:18:44 +0000

    creando el imput y el boton y la funcion para borrar la tarea

--- src/js/components/TodolistApi.jsx
+++ src/js/components/TodolistApi.jsx
@@ -19,7 +19,7 @@ export const TodolistApi = () => {
             }
         })
             .then(response => response.json()) //conviuerte la respuesta a un formato JSON
-            .then(data => { console.log(data) })  //No necesito almacenar el dato (en este caso el usuario) en ningun estado,se va a crear directamente. Tomara el dato para mostrar en la consola. 
+            .then(data => console.log(data))  //No necesito almacenar el dato (en este caso el usuario) en ningun estado,se va a crear directamente. Tomara el dato para mostrar en la consola. 
             .catch(error => {
                 console.error("Hubo un problema al crear el usuario", error); //imprimir el error enn la consola para depurar
             })
@@ -44,6 +44,68 @@ export const TodolistApi = () => {
     //    aqui va el codigo que hace algo (ejemplo el fetch)
     //       },[ cuando se repite, si esta vacio se ejecuta solo una vez al cargar la pagina, si tiene un dato se ejecuta cada vez que ese dato cambie ])
 
+    const crearTarea = async (text) => {
+         if (tarea.trim() === "") return;
+        try {
+
+            const response = 
+            await fetch(API_URL + "/todos/nahyah", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                //se envia la tarea en el body como una cadena JSON
+                body: JSON.stringify({
+                    label: text,
+                    is_done: false
+                })
+            })
+
+            if (!response.ok) {
+                throw new Error(`Error ${response.status}: No se pudo crear la tarea`)
+            }
+
+            const data = await response.json() //conviuerte la respuesta a un formato JSON
+            // console.log("tarea creada con exito:", data);
+            await obtenerLista()
+
+
+        } catch (error) {
+            console.error("Hubo un problema al crear la tarea", error);
+        }
+
+
+    }
+
+
+    //funcion para crear la tarea
+    const inputtext = (e) => {
+        //si la tecla presionada es Enter
+        if (e.key === "Enter") {
+            crearTarea(tarea) //llamar a la funcion de la API con el texto de la tarea
+            setTarea("") //limpiar el campo despues de enviar la tarea 
+        }
+    }
+
+const eliminarTarea = async (id) => {
+
+    try {
+        const response = await fetch(API_URL + "/todos/" + id, {
+            method: "DELETE"
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: No se pudo eliminar la tarea`);
+        }
+
+        // actualizar la lsita
+        await obtenerLista();
+
+    } catch (error) {
+        console.error("Hubo un problema al eliminar la tarea", error);
+    }
+}
+
 
     useEffect(() => {
         obtenerLista()
@@ -54,9 +116,23 @@ export const TodolistApi = () => {
         <div>
             <h1>Tareas de NahYah</h1>
 
+            <input type="text"
+                placeholder="Agregar tarea "
+                //  actualiza el estado tarea cada vez que el usuario escribe
+                onChange={(e) => setTarea(e.target.value)}
+                value={tarea}
+                //llama ala funcion que maneja la creacion del estado al presionar una tecla ENTER
+                onKeyDown={inputtext}
+            />
+
+
+
+            {/* hacer condicional */}
             <ol>
                 {lista.map((item) => (
-                    <li key={item.id}> {item.label}</li>
+                    <li key={item.id}> {item.label}
+                    <button onClick={() => eliminarTarea(item.id)}>X</button>
+                    </li>
                 ))}
             </ol>
         </div>
